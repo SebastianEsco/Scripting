b53a9675020083026b6b9ea428f265655181c17e
SebastianEsco <sebastian.escobarm@upb.edu.co>
2024-01-31 11:33:50 -0500

    Taller Actualizado (aun falta añadir un par de cosas)

--- Taller1/Soluciones/Calculadora/Calculadora/Program.cs
+++ Taller1/Soluciones/Calculadora/Calculadora/Program.cs
@@ -13,21 +13,21 @@
                 {
                     case 1:
                         Console.WriteLine("Ingrese un primer número");
-                        número1 = Convert.ToDouble(Console.ReadLine());
+                        número1 = RevisarSiDouble();
                         Console.WriteLine("Ingrese un segundo número que dividirá al primero");
-                        número2 = Convert.ToDouble(Console.ReadLine());
+                        número2 = RevisarSiDouble();
                         Console.WriteLine("El resultado de la operación es: " + División(número1, número2));
 
                         break;
                     case 2:
                         Console.WriteLine("Ingrese un número");
-                        número1 = Convert.ToDouble(Console.ReadLine());
+                        número1 = RevisarSiDouble();
                         Console.WriteLine("El resultado de la operación es: " + Módulo(número1));
 
                         break;
                     case 3:
                         Console.WriteLine("Ingrese un número");
-                        número1 = Convert.ToDouble(Console.ReadLine());
+                        número1 = RevisarSiDouble();
                         Console.WriteLine("El resultado de la operación es: " + Cuadrado(número1));
 
                         break;
@@ -47,13 +47,22 @@
                     Console.WriteLine("Módulo (oprima 2)");
                     Console.WriteLine("Cuadrado (oprima 3)");
                     Console.WriteLine("Salir (Ingrese -1 en el teclado para salir)");
-                    opción = Convert.ToInt32(Console.ReadLine());
+                    opción = RevisarSiInt();
                 }
                 double División(double num1, double num2)
                 {
-                    double resultado;
-                    resultado = num1 / num2;
-                    return resultado;
+                    if(num2 != 0)
+                    {
+                        double resultado;
+                        resultado = num1 / num2;
+                        return resultado;
+                    }
+                    else
+                    {
+                        Console.WriteLine("División por 0 no es valida.");
+                        return 0;
+                    }
+                    
                 }
                 double Módulo(double num1)
                 {
@@ -71,6 +80,57 @@
                 }
             }
             while (opción != -1);
+
+        }
+
+        static int RevisarSiInt()
+        {
+            while (true)
+            {
+                string temp = Console.ReadLine();
+
+                try
+                {
+                    Convert.ToInt32(temp);
+                    if (Convert.ToInt32(temp) != 0)
+                    {
+                        return Convert.ToInt32(temp);
+                    }
+                    else
+                    {
+                        Console.WriteLine("No escribió un número o escribió una opción no válida");
+                    }
+
+                }
+                catch
+                {
+                    Console.WriteLine("No escribió un número o escribió escribió una opción no válida");
+
+                }
+            }
+
+
+        }
+
+
+        static double RevisarSiDouble()
+        {
+            while (true)
+            {
+                string temp = Console.ReadLine();
+
+                try
+                {
+                    Convert.ToDouble(temp);
+                    return Convert.ToDouble(temp);
+                }
+                catch
+                {
+                    Console.WriteLine("No escribió un número o escribió un numero menor o igual a 0, vuelva a intentarlo");
+
+                }
+            }
+
 
         }
 
---
+++ Taller1/Soluciones/ConcatenarMatriz/ConcatenarMatriz/Program.cs
@@ -0,0 +1,112 @@
+﻿namespace ConcatenarMatriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] matriz;
+            int n, m;
+
+
+            n = RevisarSiInt("ancho");
+            m = RevisarSiInt("largo");
+
+            matriz = new string[n, m];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.WriteLine("Escriba la letra para la posición " + i + " , " + j + " de la matriz");
+                    matriz[i, j] = RevisarSiString();
+                }
+            } //Rellenar la matriz
+
+            Console.WriteLine("\n\nMatriz: \n");
+            MostrarMatriz(matriz, n, m);
+
+            Console.WriteLine("\n\nMatriz Concatenada: " + ConcatenarMatriz(matriz, n, m));
+
+
+        }
+
+        static int RevisarSiInt(string opcion)
+        {
+            Console.WriteLine("Escriba el " + opcion + " de la matriz (numero entero mayor a 0)");
+
+            while (true)
+            {
+                string temp = Console.ReadLine();
+
+                try
+                {
+                    Convert.ToInt32(temp);
+                    if (Convert.ToInt32(temp) != 0)
+                    {
+                        return Convert.ToInt32(temp);
+                    }
+                    else
+                    {
+                        Console.WriteLine("No escribió un número o escribió un numero menor o igual a 0, vuelva a intentarlo");
+                    }
+
+                }
+                catch
+                {
+                    Console.WriteLine("No escribió un número o escribió un numero menor o igual a 0, vuelva a intentarlo");
+
+                }
+            }
+
+
+        }
+
+        static string RevisarSiString()
+        {
+            while (true)
+            {
+                string temp = Console.ReadLine();
+
+                try
+                {
+                    Convert.ToInt32(temp);
+                    Console.WriteLine("No escribió una letra sino un número, vuelva a intentarlo");
+                }
+                catch
+                {
+                    return temp;
+
+                }
+            }
+
+
+        }
+
+        static void MostrarMatriz(string[,] matriz, int n, int m)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(matriz[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static string ConcatenarMatriz(string[,] matriz, int n, int m)
+        {
+            string matrizConcatenada = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrizConcatenada += matriz[i, j];
+                }
+
+            }
+            return matrizConcatenada;
+        }
+    }
+}
